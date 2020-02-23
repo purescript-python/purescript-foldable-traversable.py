2830fb7418648ad9bbd965b6a2263c1d2978cecb
thautawarm <twshere@outlook.com>
2020-02-23 21:11:52 +0900

    up

--- python-ffi/Data/FunctorWithIndex.py
+++ python-ffi/Data/FunctorWithIndex.py
@@ -1,6 +1,5 @@
 def mapWithIndexArray(f):
     # This is a trick for speed up.
     # CPython details:
-    #   loading default argument is about 10% faster than freevars
-    # in CPython.
+    #   loading default argument is about 10% faster than freevars.
     return lambda xs, f=f: tuple(f(i)(x) for i, x in enumerate(xs))
